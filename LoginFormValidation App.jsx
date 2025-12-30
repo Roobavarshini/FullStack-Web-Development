735ca1471c937c4634443eb49d87ca242fdc56ee
Roobavarshini M <roobavarshini.m@gmail.com>
2025-12-30 09:17:48 +0530

    Delete LoginFormValidation App.jsx

--- LoginFormValidation App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LoginFormValidation from "./FullStack/Sample/LoginFormValidation";
-
-function App() {
-  return <LoginFormValidation />;
-}
-
-export default App;
